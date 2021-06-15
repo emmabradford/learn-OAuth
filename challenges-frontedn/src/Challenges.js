73296c6eeb769db5c44c5566647afd2e9fb1af9b
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-15 11:53:51 -0500

    added chalenges class

---
+++ challenges-frontedn/src/Challenges.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Challenges.css";
+
+const Challenges = ()=>{
+    return(
+        <div className="Challenges-body">
+            <h3>
+                challenges
+            </h3>
+            <h5 className="Content">
+
+            </h5>
+        </div>
+    );
+}
