33eeea84da6a3d3545f8715edfa0e723e8d2b2ed
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-15 11:49:15 -0500

    added logout button

---
+++ challenges-frontedn/src/auth/LogoutButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LogoutButton = ()=> {
+    const logout = async ()=> {
+
+    };
+
+    return(
+        <button className='Logout-buttin' onClick={()=> logout()}>
+            logout
+        </button>
+    );
+};
+
+export default LogoutButton;
